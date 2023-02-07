4b3ae4753502db799bb061a9d39ec940aea1a82a
cheeseHxzz <124635270+cheeseHxzz@users.noreply.github.com>
2023-02-06 19:54:23 -0500

    Update cider.tsx

--- src/plugins/cider.tsx
+++ src/plugins/cider.tsx
@@ -17,18 +17,20 @@
 */
 
 import definePlugin from "@utils/types";
+import { Forms } from "@webpack/common";
+import { Link } from "@components/Link";
 
 const appIds = [
     "911790844204437504",
     "886578863147192350",
     "1020414178047041627",
     "1032800329332445255",
-	"351365527874174977"
+    "351365527874174977"
 ]
 
 export default definePlugin({
-    name: "richerCider", // why must you make me run in circles for 2 hours.
-    description: "Enhances Cider by adding the \"Listening to\" and progress bar to the rich presence.",
+    name: "richerCider",
+    description: "Enhances Cider (More details in info button) by adding the \"Listening to\" type prefix to the user's rich presence when an applicable ID is found.",
     authors: [{
         id: 191621342473224192n,
         name: "cryptofyre",
@@ -42,10 +44,29 @@ export default definePlugin({
             }
         }
     ],
+    
+    settingsAboutComponent: () => (
+        <>
+            <Forms.FormTitle tag="h3">Install Cider to use this Plugin</Forms.FormTitle>
+            <Forms.FormText>
+                <Link href="https://cider.sh">Follow the link to our website</Link> to get Cider up and running, and then enable the plugin.
+            </Forms.FormText>
+            <br></br>
+            <Forms.FormTitle tag="h3">What is Cider?</Forms.FormTitle>
+            <Forms.FormText>
+                Cider is an open-source and community oriented Apple Music client for Windows, macOS, and Linux.
+            </Forms.FormText>
+            <br></br>
+            <Forms.FormTitle tag="h3">Recommended Optional Plugins</Forms.FormTitle>
+            <Forms.FormText>
+                I'd recommend using TimeBarAllActivities alongside this plugin to give off a much better visual to the eye (Keep in mind this only effects your client and will not show on the serverside)
+            </Forms.FormText>
+        </>
+    ),
 
     patchActivity(activity: any) {
         if (appIds.includes(activity.application_id)) {
             activity.type = 2; /* LISTENING type */
         }
-    }
+    },
 });
